e6ba5e2c9f00916a9216c7993e5a40d91a401e48
Noa Dudai <nooninana@gmail.com>
2021-06-23 17:38:57 +0300

    added a 'show all users' flask function

--- sourdough/db/tests.py
+++ sourdough/db/tests.py
@@ -11,14 +11,23 @@ def create_account():
     name = request.args.get('name')
     last_name = request.args.get('last_name')
     email = request.args.get('email')
-    user = User(name=name, last_name=last_name, email=email)
+    my_user = User(name=name, last_name=last_name, email=email)
     session = Session()
-    session.add(user)
+    session.add(my_user)
     session.commit()
     return "created"
 
 
-Base.metadata.create_all(engine)
+@app.route('/show_all_users')
+def show_all_users():
+    session = Session()
+    users = session.query(User).all()
+    return_list = [str(user) for user in users]
+    return_str = str(return_list)
+    return jsonify(return_str)
+
+
+# Base.metadata.create_all(engine)
 
 
 if __name__ == '__main__':
---
+++ sourdough/server/app.py
@@ -0,0 +1,29 @@
+from sourdough.db.models.user_table import User
+from sourdough.db.orm_config import Base, engine, Session
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/create_account')
+def create_account():
+    name = request.args.get('name')
+    last_name = request.args.get('last_name')
+    email = request.args.get('email')
+    user = User(name=name, last_name=last_name, email=email)
+    session = Session()
+    session.add(user)
+    session.commit()
+    return "created"
+
+
+@app.route('/show_all_users')
+def show_all_users():
+    session = Session()
+    users = session.query(User).all()
+    return_list = [str(user) for user in users]
+    return_str = str(return_list)
+    return jsonify(return_str)
+
+
+Base.metadata.create_all(engine)
