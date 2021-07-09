f3d9a255e1864ed1bfb233ff03d3630d2a406363
Noa Dudai <nooninana@gmail.com>
2021-07-09 14:27:14 +0300

    created working conftest

---
+++ sourdough/db/models/__init__.py
@@ -0,0 +1,6 @@
+import sourdough.db.models.feeding_actions_table
+import sourdough.db.models.leaven_extractions_table
+import sourdough.db.models.refrigerator_actions_table
+import sourdough.db.models.sourdough_table
+import sourdough.db.models.sourdough_targets_table
+import sourdough.db.models.user_table
