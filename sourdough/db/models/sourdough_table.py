f5667f0e6295c4e3a7414ca809bde1c86bfd13fa
Noa Dudai <nooninana@gmail.com>
2021-07-22 23:09:44 +0300

    Add useful sourdough properties

--- sourdough/db/models/sourdough_table.py
+++ sourdough/db/models/sourdough_table.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from sqlalchemy import Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from sourdough.db.orm_config import Base
@@ -9,6 +11,7 @@ class SourdoughModel(Base):
     __tablename__ = 'sourdough_starters'
     id = Column(Integer, primary_key=True, nullable=False)
     max_maintenance_weight = Column(Integer, default=100)
+    min_maintenance_weight = Column(Integer, default=4)
 
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     user = relationship("UserModel", back_populates="sourdoughs")
@@ -17,7 +20,43 @@ class SourdoughModel(Base):
     refrigerator_actions = relationship("RefrigeratorActionModel", uselist=True, back_populates="sourdough")
     sourdough_targets = relationship("SourdoughTargetModel", uselist=True, back_populates="sourdough")
 
-# A function to calculate and return the weight of the sourdough starter.
+    @property
+    def last_refrigerator_action(self):
+        if len(self.refrigerator_actions) == 0:
+            raise Exception("Unknown sourdough refrigeration state")
+        return self.refrigerator_actions[-1]
+
+    @property
+    def upcoming_sourdough_targets(self):
+        now = datetime.datetime.now()
+        return [target for target in self.sourdough_targets if target.date_of_action > now]
+
+    @property
+    def has_upcoming_targets(self):
+        return len(self.upcoming_sourdough_targets) != 0
+
+    @property
+    def next_sourdough_target(self):
+        if not self.has_upcoming_targets:
+            raise Exception("No upcoming sourdough targets")
+        targets_sorted_by_date = sorted(self.upcoming_sourdough_targets, key=lambda target: target.date_of_action)
+        return targets_sorted_by_date[0]
+
+    @property
+    def extracted_today(self):
+        if not self.extractions:
+            return False
+        last_extraction = self.extractions[-1]
+        return last_extraction.days_from_today < 1
+
+    @property
+    def fed_today(self):
+        if not self.feeding_actions:
+            return False
+        last_feeding = self.feeding_actions[-1]
+        return last_feeding.days_from_today < 1
+
+    # A function to calculate and return the weight of the sourdough starter.
     @property
     def weight(self):
         actions = []
@@ -34,32 +73,25 @@ class SourdoughModel(Base):
                 sourdough_starter_weight -= int(action.sourdough_weight_used_in_grams)
         return sourdough_starter_weight
 
-# A function to calculate and return how many days the sourdough starter has been in the refrigerator.
+    # A function to calculate and return how many days the sourdough starter has been in the refrigerator.
     @property
-    def days_in_refrigerator(self):
-        my_refrigerator = self.refrigerator_actions
-        today = datetime.datetime.today().date()
-        refrigerator_date = my_refrigerator[-1].date_of_action.date()
-        delta = refrigerator_date - today
-        return delta.days
+    def days_in_refrigerator(self) -> int:
+        return self.last_refrigerator_action.days_from_today
 
-# A function that returns if the sourdough starter is in the refrigerator.
+    # A function that returns if the sourdough starter is in the refrigerator.
     @property
-    def is_in_refrigerator(self):
-        my_refrigerator_action = self.refrigerator_actions
-        return my_refrigerator_action[-1].in_or_out
+    def in_refrigerator(self) -> bool:
+        return self.last_refrigerator_action.in_or_out == "in"
 
-# A function to calculate and return how many days there are until the sourdough target date.
+    # A function to calculate and return how many days there are until the sourdough target date.
     @property
-    def days_until_target(self):
-        my_target_date = self.sourdough_targets
+    def days_until_target(self) -> int:
         today = datetime.datetime.today().date()
-        target = my_target_date[-1].date_of_action.date()
+        target = self.next_sourdough_target.date_of_action.date()
         delta = target - today
         return delta.days
 
-# A function to check if the sourdough starter weight is over the maintenance weight.
+    # A function to check if the sourdough starter weight is over the maintenance weight.
     @property
-    def is_over_maintenance_weight(self):
+    def is_over_maintenance_weight(self) -> bool:
         return self.weight < self.max_maintenance_weight
-
